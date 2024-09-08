d05f421dd5f448a19839c723bf18a15da49b8a26
Saul Burgos <saulburgos6@gmail.com>
2024-09-07 21:31:00 -0400

    add variables and better error manages

--- typeScript/repository.ts
+++ typeScript/repository.ts
@@ -1,19 +1,37 @@
 import * as fs from "fs/promises";
+import { Note } from "./note.model";
 export class NoteRepository {
+  private fileLocaltion = "./";
+  private fileName = "db.json";
+  private fileReferes = `${this.fileLocaltion}${this.fileName}`;
   constructor() {
-    fs.readFile("./db.txt", "utf8").catch((err) => {
-      if (err) {
-        fs.writeFile("./db.txt", "", "utf8");
+    this.initializeFile();
+  }
+  private async initializeFile() {
+    try {
+      await fs.readFile(this.fileReferes, "utf8");
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await fs.writeFile(this.fileReferes, "[]", "utf8");
+        console.log("Archivo creado con contenido inicial vacío.");
+      } else {
+        console.error("Error inesperado al leer el archivo:", err);
+        throw err;
       }
-    });
+    }
   }
 
-  async save(data: string): Promise<void> {
-    await fs.appendFile("./db.txt", `${data}\n`, "utf8");
+  async save(data: Note): Promise<void> {
+    const dataDb = await fs.readFile(this.fileReferes, "utf8");
+    const dataTosave: Note[] = dataDb.trim() ? JSON.parse(dataDb) : [];
+    dataTosave.push(data);
+    const jsonString = JSON.stringify(dataTosave, null, 2);
+
+    await fs.writeFile(this.fileReferes, `${jsonString}\n`, "utf8");
   }
 
-  async findAll(): Promise<string> {
-    const file = await fs.readFile("./db.txt");
-    return file.toString();
+  async findAll(): Promise<Note[]> {
+    const file = await fs.readFile(this.fileReferes, "utf8");
+    return JSON.parse(file);
   }
 }
